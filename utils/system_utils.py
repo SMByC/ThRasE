d15b7576adb37a35256d7c62193e36c1200b5279
Xavier Corredor Llano <xcorredorl@ideam.gov.co>
2019-02-15 13:17:03 -0500

    Added system utils functions

---
+++ utils/system_utils.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ ThRasE
+                                 A QGIS plugin
+ ThRasE is a Qgis plugin for Thematic Raster Edition
+                              -------------------
+        copyright            : (C) 2019 by Xavier Corredor Llano, SMByC
+        email                : xcorredorl@ideam.gov.co
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
+import functools
+import traceback
+import os, sys, subprocess
+
+from qgis.PyQt.QtCore import Qt
+from qgis.PyQt.QtWidgets import QApplication
+from qgis.PyQt.QtGui import QCursor
+from qgis.core import QgsMessageLog, Qgis
+from qgis.utils import iface
+
+
+def error_handler(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except Exception as e:
+            # restore mouse
+            QApplication.restoreOverrideCursor()
+            QApplication.processEvents()
+            # message in status bar
+            msg_error = "An error has occurred in ThRasE plugin. " \
+                        "See more in Qgis log messages panel."
+            iface.messageBar().pushMessage("ThRasE", msg_error,
+                                           level=Qgis.Critical, duration=10)
+            # message in log
+            msg_error = "\n################## ERROR IN ThRasE PLUGIN:\n"
+            msg_error += traceback.format_exc()
+            msg_error += "\nPlease report the error in:\n" \
+                         "\thttps://bitbucket.org/smbyc/qgisplugin-thrase/issues"
+            msg_error += "\n################## END REPORT"
+            QgsMessageLog.logMessage(msg_error)
+    return wrapper
+
+
+def wait_process(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        # mouse wait
+        QApplication.setOverrideCursor(QCursor(Qt.WaitCursor))
+        # do
+        obj_returned = func(*args, **kwargs)
+        # restore mouse
+        QApplication.restoreOverrideCursor()
+        QApplication.processEvents()
+        # finally return the object by f
+        return obj_returned
+    return wrapper
+
+
+def open_file(filename):
+    """Open a file with the standard application"""
+    filename = os.path.abspath(filename)
+
+    if sys.platform == "linux" or sys.platform == "linux2":
+        # Linux
+        subprocess.call(["xdg-open", filename])
+    elif sys.platform == "darwin":
+        # OS X
+        subprocess.call(["open", filename])
+    elif sys.platform == "win32":
+        # Windows
+        os.startfile(filename)
+
+
+class block_signals_to(object):
+    """Block all signals emits from specific QT object"""
+    def __init__(self, object_to_block):
+        self.object_to_block = object_to_block
+
+    def __enter__(self):
+        # block
+        self.object_to_block.blockSignals(True)
+
+    def __exit__(self, type, value, traceback):
+        # unblock
+        self.object_to_block.blockSignals(False)
+
