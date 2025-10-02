5b105c028b622435101d606bf6fd58c5019eca7d
Xavier C. Llano <xavier.corredor.llano@gmail.com>
2025-10-02 13:20:45 -0700

    Remove pre-initialization of extra_deps not needed

--- __init__.py
+++ __init__.py
@@ -29,36 +29,6 @@
 """
 
 
-from qgis.PyQt.QtCore import QCoreApplication
-from qgis.PyQt.QtWidgets import QMessageBox
-
-from ThRasE.utils.extra_deps import load_install_extra_deps, WaitDialog
-
-
-def pre_init_plugin(iface):
-    app = QCoreApplication.instance()
-    parent = iface.mainWindow()
-    dialog = None
-    log = ''
-    try:
-        for msg_type, msg_val in load_install_extra_deps():
-            app.processEvents()
-            if msg_type == 'log':
-                log += msg_val
-            elif msg_type == 'needs_install':
-                dialog = WaitDialog(parent, 'ThRasE - installing dependencies')
-            elif msg_type == 'install_done':
-                dialog.accept()
-    except Exception as e:
-        if dialog:
-            dialog.accept()
-        QMessageBox.critical(parent, 'ThRasE - installing dependencies',
-                             'An error occurred during the installation of Python packages. ' +
-                             'Click on "Stack Trace" in the QGIS message bar for details.')
-        raise RuntimeError('\nThRasE: Error installing Python packages. Read install instruction: '
-                           'https://smbyc.github.io/ThRasE\nLog:\n' + log) from e
-
-
 # noinspection PyPep8Naming
 def classFactory(iface):  # pylint: disable=invalid-name
     """Load ThRasE class from file ThRasE.
@@ -66,9 +36,5 @@ def classFactory(iface):  # pylint: disable=invalid-name
     :param iface: A QGIS interface instance.
     :type iface: QgsInterface
     """
-    # load/install extra python dependencies
-    #pre_init_plugin(iface)
-
-    #
     from .thrase import ThRasE
     return ThRasE(iface)
