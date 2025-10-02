c280451cba48135de9edd02cb56f30ac782e545e
Xavier C. Llano <xavier.corredor.llano@gmail.com>
2025-10-02 12:58:14 -0700

    Trying to fix double free or corruption in tests

--- .docker/run-docker-tests.sh
+++ .docker/run-docker-tests.sh
@@ -4,5 +4,5 @@ set -e
 
 pushd /usr/src
 DEFAULT_PARAMS='./ThRasE/tests/ -v --qgis_disable_gui --qgis_disable_init'
-xvfb-run pytest ${@:-`echo $DEFAULT_PARAMS`}
+xvfb-run -a -s "-screen 0 1920x1080x24" pytest ${@:-`echo $DEFAULT_PARAMS`}
 popd
